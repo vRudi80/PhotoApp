1b01e778048087db3b39b7a435d23b4695699d20
vRudi80 <kovari.rudolf@gmail.com>
2026-04-30 11:05:04 +0200

    Create App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      height: '100vh',
+      fontFamily: 'sans-serif',
+      backgroundColor: '#121212',
+      color: 'white'
+    }}>
+      <h1>PhotoApp - Hello World! 📸</h1>
+      <p>A frontend sikeresen fut a Vercelen.</p>
+    </div>
+  )
+}
+
+export default App
