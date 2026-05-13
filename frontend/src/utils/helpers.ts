965eb37f2035ecbb6dc826b8db834654face6e07
vRudi80 <kovari.rudolf@gmail.com>
2026-05-13 15:46:03 +0200

    Update helpers.ts

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -1,11 +1,11 @@
-// Zászló generáló
+// Okosított zászló generáló
 export function getFlagEmoji(countryCode: string) {
   if (!countryCode || countryCode.length !== 2) return null;
   const codePoints = countryCode.toUpperCase().split('').map(char =>  127397 + char.charCodeAt(0));
   return String.fromCodePoint(...codePoints);
 }
 
-// Drive vagy URL kép beolvasó
+// Központi kép URL generáló Google Drive-hoz
 export function getImageUrl(driveFileId?: string | null, fileUrl?: string) {
   if (driveFileId) {
     return `https://lh3.googleusercontent.com/d/$${driveFileId}`;
@@ -13,8 +13,8 @@ export function getImageUrl(driveFileId?: string | null, fileUrl?: string) {
   return fileUrl || '';
 }
 
-// YouTube link átalakító beágyazáshoz
-export const getYouTubeEmbed = (url: string) => {
+// YouTube URL átalakító
+export function getYouTubeEmbed(url: string) {
   const match = url.match(/(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))([^&?]+)/);
   return match ? `https://www.youtube.com/embed/${match[1]}` : url;
-};
+}
