0c181b1ee0429bf669a69fc225c16d048de795ea
vRudi80 <kovari.rudolf@gmail.com>
2026-05-13 15:45:40 +0200

    Create constants.ts

---
+++ frontend/src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const GOOGLE_CLIENT_ID = "197361744572-ih728hq5jft3fqfd1esvktvrd8i97kcp.apps.googleusercontent.com";
+export const BACKEND_URL = "https://photoapp-backend-m4d1.onrender.com"; 
+export const ADMIN_EMAIL = "kovari.rudolf@gmail.com";
