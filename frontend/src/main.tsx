90efcd664ee7791e31552ce71a26a1f46f70bb9d
Rudolf <kovari.rudolf@gmail.com>
2026-05-30 00:09:30 +0200

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { GoogleOAuthProvider } from '@react-oauth/google'
-
-
-const GOOGLE_CLIENT_ID = "A_TE_CLIENT_ID_KÓDOD.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>,
 )
