672436ca66dcb1286e3d2ee1a2b8ae286bf58614
Rudolf <kovari.rudolf@gmail.com>
2026-05-30 00:05:14 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -818,8 +818,8 @@ const fetchMyEntries = async (email: string) => {
     return matchName || matchPatron;
   });
 
-  return (
-  
+    return (
+    <>
       {fullscreenData && (
         <FullscreenModal 
           data={fullscreenData} 
@@ -855,7 +855,6 @@ const fetchMyEntries = async (email: string) => {
             onLogout={() => { localStorage.removeItem('photoAppToken'); setUser(null); }} 
           />
           
-          {/* JAVÍTÁS: A dupla <main> taget kivettük, így szép tiszta a kódstruktúra */}
           <main className="app-main">
             {activeTab === 'dashboard' && (
               <DashboardView 
@@ -1078,7 +1077,7 @@ const fetchMyEntries = async (email: string) => {
           
         </div>
       )}
-
+    </>
   );
 }
 
