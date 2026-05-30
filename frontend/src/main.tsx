6b747b38805a2850bf57cb193715e5bbd76a8cd0
Rudolf <kovari.rudolf@gmail.com>
2026-05-30 06:55:02 +0200

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,9 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
