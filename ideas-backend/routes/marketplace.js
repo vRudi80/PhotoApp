911b0d5af3670ad997e220228ac0a7ecb7d16319
Rudolf <kovari.rudolf@gmail.com>
2026-06-18 09:52:56 +0200

    Update marketplace.js

--- ideas-backend/routes/marketplace.js
+++ ideas-backend/routes/marketplace.js
@@ -1,13 +1,13 @@
 const cloudinary = require('cloudinary').v2;
 
 // Ha az index.js-ben még nincs konfigurálva a Cloudinary, akkor itt (vagy a főfájlban) tedd meg:
-/*
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
-*/
+
 
 module.exports = function(app, pool, checkPremium) {
 
