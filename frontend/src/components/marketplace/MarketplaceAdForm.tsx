421fef1270d958e8e3dcf006b324d8d62a074681
Rudolf <kovari.rudolf@gmail.com>
2026-06-18 10:17:40 +0200

    Update MarketplaceAdForm.tsx

--- frontend/src/components/marketplace/MarketplaceAdForm.tsx
+++ frontend/src/components/marketplace/MarketplaceAdForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { BACKEND_URL } from '../utils/constants'; // 🎯 ÚJ: Backend URL importálása
+import { BACKEND_URL } from '../../../utils/constants';
 
 type Category = 'camera' | 'lens' | 'lighting' | 'drone' | 'accessory' | '';
 type ConditionState = 'mint' | 'excellent' | 'good' | 'heavily_used' | 'for_parts';
