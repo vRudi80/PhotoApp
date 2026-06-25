7689cb58e0ab1af00fa59539f9c0e6ed9b3122a7
Rudolf <kovari.rudolf@gmail.com>
2026-06-25 08:22:33 +0200

    Fix podcast route import in index.js

--- ideas-backend/index.js
+++ ideas-backend/index.js
@@ -147,7 +147,7 @@ require('./routes/payments')(app, pool, stripe);
 require('./routes/clubs')(app, pool, drive, upload, cleanupTempFile);
 require('./routes/salons')(app, pool, checkPremium, genAI, xlsx, cheerio, upload, cleanupTempFile);
 require('./routes/tickets')(app, pool);
-require('./podcast')(app, pool);
+require('./routes/podcast')(app, pool);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Szerver fut a ${PORT} porton`));
