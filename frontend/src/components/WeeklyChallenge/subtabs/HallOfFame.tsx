6b4bfc9c256471bacd1d69100fb08dc138d3c4c2
Rudolf <kovari.rudolf@gmail.com>
2026-06-28 14:36:25 +0200

    Fix import path for LanguageContext in HallOfFame.tsx

--- frontend/src/components/WeeklyChallenge/subtabs/HallOfFame.tsx
+++ frontend/src/components/WeeklyChallenge/subtabs/HallOfFame.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useLanguage } from '../../context/LanguageContext';
+import { useLanguage } from '../../../context/LanguageContext';
 
 interface HallOfFameUser {
   user_name: string;
