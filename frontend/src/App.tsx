c989583ea6e1f17c3c0063dd6bb6601f6b4dd17b
Rudolf <kovari.rudolf@gmail.com>
2026-07-03 13:52:58 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -474,8 +474,8 @@ function MainContent() {
   };
 
   const saveUserClub = async (email: string) => { 
-    const clubName = userClubEdits[email] !== undefined ? userClubEdits[email] : (Array.isArray(allUsers) ? (allUsers.find(u => u.email === email)?.club_name || '') ; ''); 
-    const clubRole = userRoleEdits[email] !== undefined ? userRoleEdits[email] : (Array.isArray(allUsers) ? (allUsers.find(u => u.email === email)?.club_role || 'member');
+    const clubName = userClubEdits[email] !== undefined ? userClubEdits[email] : (Array.isArray(allUsers) ? (allUsers.find(u => u.email === email)?.club_name || '') : ''); 
+    const clubRole = userRoleEdits[email] !== undefined ? userRoleEdits[email] : (Array.isArray(allUsers) ? (allUsers.find(u => u.email === email)?.club_role || 'member') : 'member');
     
     const matchedClub = Array.isArray(clubs) ? clubs.find(c => c.name === clubName) : null;
     const clubId = matchedClub ? matchedClub.id : null;
